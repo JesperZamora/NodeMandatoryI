06d824814b2687525e99d603ad5636799d962f11
JesperZamora <jesp78m6@stud.kea.dk>
2024-03-18 00:07:12 +0100

    first commit - made express server with routing and a structured generic layout

---
+++ router/pagesRouter.js
@@ -0,0 +1,58 @@
+import { 
+  homepagePage, 
+  introdutionPage, 
+  firstServerPage, 
+  servingHtmlFilesPage, 
+  timePage, 
+  exportImportPage, 
+  ssrPage, 
+  contactPage
+} from "../util/readPages.js";
+
+import { Router } from "express"
+const router = Router();
+
+
+//---------- HTML ----------//
+
+// Homepage
+router.get("/", (req, res) => {
+  res.send(homepagePage);
+});
+
+// Introduction
+router.get("/introduction", (req, res) => {
+  res.send(introdutionPage);
+});
+
+// First Server
+router.get("/firstserver", (req, res) => {
+  res.send(firstServerPage);
+});
+
+// Serving HTML files
+router.get("/servinghtmlfiles", (req, res) => {
+  res.send(servingHtmlFilesPage);
+}); 
+
+// Time
+router.get("/time", (req, res) => {
+  res.send(timePage);
+}); 
+
+// Export / Import
+router.get("/exportimport", (req, res) => {
+  res.send(exportImportPage);
+}); 
+
+// SSR
+router.get("/ssr", (req, res) => {
+  res.send(ssrPage);
+}); 
+
+// Contact
+router.get("/contact", (req, res) => {
+  res.send(contactPage);
+}); 
+
+export default router;
---
+++ util/readPages.js
@@ -0,0 +1,41 @@
+import { readPage, renderPage } from "./templaitingEngine.js";
+
+const homepage = readPage("./public/pages/homepage/homepage.html");
+export const homepagePage = renderPage(homepage, {
+  tabTitle: "Homepage"
+});
+
+const introduction = readPage("./public/pages/introduction/introduction.html");
+export const introdutionPage = renderPage(introduction, {
+  tabTitle: "introduction"
+});
+
+const firstServer = readPage("./public/pages/first_server/first_server.html");
+export const firstServerPage = renderPage(firstServer, {
+  tabTitle: "First Server"
+});
+
+const servingHtmlFiles = readPage("./public/pages/serving_html_files/serving_html_files.html");
+export const servingHtmlFilesPage = renderPage(servingHtmlFiles, {
+  tabTitle: "Serving Html Files"
+});
+
+const time = readPage("./public/pages/time/time.html");
+export const timePage = renderPage(time, {
+  tabTitle: "Time"
+});
+
+const exportImport = readPage("./public/pages/export_import/export_import.html");
+export const exportImportPage = renderPage(exportImport, {
+  tabTitle: "Export Import"
+});
+
+const ssr = readPage("./public/pages/ssr/ssr.html");
+export const ssrPage = renderPage(ssr, {
+  tabTitle: "Server Side Rendering"
+});
+
+const contact = readPage("./public/pages/contact/contact.html", {
+  tabTitle: "Contacts"
+});
+export const contactPage = renderPage(contact);
